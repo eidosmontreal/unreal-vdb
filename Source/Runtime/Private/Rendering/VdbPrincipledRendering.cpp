9674acaf4e76e062a267d834a24e00819c0fde42
Thibault Lambert <tlambert.cg@gmail.com>
2023-03-01 08:58:43 -0500

    Fix face culling with negative scaling transforms
    People that want to use the exact same transform as their DCC use negative scaling. It caused volume to disappear when going inside. 
    Now fixed by checking reverse culling.

--- Source/Runtime/Private/Rendering/VdbPrincipledRendering.cpp
+++ Source/Runtime/Private/Rendering/VdbPrincipledRendering.cpp
@@ -348,7 +348,7 @@ void FVdbPrincipledRendering::Render_RenderThread(FPostOpaqueRenderParameters& P
 					FGraphicsPipelineStateInitializer GraphicsPSOInit;
 					RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
 					GraphicsPSOInit.BlendState = TStaticBlendState<CW_RGBA, BO_Add, BF_One, BF_Zero, BO_Add, BF_One, BF_Zero>::GetRHI();
-					GraphicsPSOInit.RasterizerState = GetStaticRasterizerState<true>(FM_Solid, CM_CCW);
+					GraphicsPSOInit.RasterizerState = GetStaticRasterizerState<true>(FM_Solid, Proxy->IsLocalToWorldDeterminantNegative() ? CM_CW : CM_CCW);
 					GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Less>::GetRHI();
 					GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GFilterVertexDeclaration.VertexDeclarationRHI;
 					GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
